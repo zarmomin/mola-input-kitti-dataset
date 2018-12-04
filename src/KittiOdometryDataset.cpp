41903e7ef86b5f996d6e25251f7cda96077b6b09
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-12-04 08:21:29 +0100

    wip temp commit

--- src/KittiOdometryDataset.cpp
+++ src/KittiOdometryDataset.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <mola-sensor-kitti-dataset/KittiOdometryDataset.h>
+#include <mrpt/system/CDirectoryExplorer.h>
 #include <mrpt/system/filesystem.h>  //ASSERT_DIRECTORY_EXISTS_()
 #include <yaml-cpp/yaml.h>
 
@@ -44,13 +45,31 @@ void KittiOdometryDataset::initialize(const std::string& cfg_block)
 
     ENSURE_YAML_ENTRY_EXISTS(params, "sequence");
     replay_selected_seq_ = params["sequence"].as<std::string>();
-    ASSERT_DIRECTORY_EXISTS_(
-        kitti_basedir_ + "/sequences/" + replay_selected_seq_);
+    const std::string seq_dir =
+        kitti_basedir_ + "/sequences/" + replay_selected_seq_;
+    ASSERT_DIRECTORY_EXISTS_(seq_dir);
 
     // Optional params with default values:
     time_warp_scale_ = params["time_warp_scale"].as<double>(time_warp_scale_);
     publish_lidar_   = params["publish_lidar"].as<bool>(publish_lidar_);
-    publish_stereo_  = params["publish_stereo"].as<bool>(publish_stereo_);
+    for (unsigned int i = 0; i < 4; i++)
+        publish_image_[i] =
+            params[mrpt::format("publish_image_%u", i)].as<bool>(
+                publish_image_[i]);
+
+    // Make list of all existing files and preload everything we may need later
+    // to quickly replay the dataset in realtime:
+
+    // Odometry datasets:
+    // Images  : "000000.png"
+    // Velodyne: "001092.bin"
+    // Raw datasets:
+    // Velodyne: "0000000000.bin"
+    // Images  : "0000000000.png"
+    // Non-uniform format. Solution: make a list of files and sort them.
+    using direxpl = mrpt::system::CDirectoryExplorer;
+    direxpl::TFileInfoList lstFiles;
+    direxpl::explore(seq_dir + "velodyne", FILE_ATTRIB_ARCHIVE, lstFiles);
 
     MRPT_END
 }
